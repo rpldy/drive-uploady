4c100a81d393fffd560d0ec95de22590faf57322
yoavniran <yoavniran@gmail.com>
2020-11-17 00:07:59 +0200

    add action logger with uploady hooks

--- stories/DriveUploady.stories.js
+++ stories/DriveUploady.stories.js
@@ -1,21 +1,31 @@
 import React from "react";
 import UploadButton from "@rpldy/upload-button";
+import { useItemStartListener, useItemFinishListener } from "@rpldy/uploady";
 import DriveUploady from "../src";
 
 import readme from "../README.md";
 
-export const Simple = ({ clientId, scope, folder }) => {
+const UploadyActionLogger = ({ onItemStart, onItemFinish }) => {
+  useItemStartListener(onItemStart);
+  useItemFinishListener(onItemFinish);
+
+  return null;
+};
+
+export const Simple = ({ clientId, scope, folder, onItemStart, onItemFinish }) => {
   clientId = clientId || process.env.DRIVE_CLIENT_ID || "";
 
   return <DriveUploady
     debug
     clientId={clientId}
     scope={scope}
-    params={{ parents: [folder] }}
+    params={{ parents: folder ? [folder] : undefined }}
   >
     <h2>Drive Uploady</h2>
 
     <UploadButton>Upload to Drive</UploadButton>
+
+    <UploadyActionLogger onItemStart={onItemStart} onItemFinish={onItemFinish}/>
   </DriveUploady>;
 };
 
@@ -36,6 +46,8 @@ export default {
   argTypes: {
     clientId: { description: "The API client Id. Obtained from the Google dev console" },
     scope: { description: "The scope your app requires" },
-    folder: { description: "G-Drive Folder ID (not a Prop!)"}
+    folder: { description: "G-Drive Folder ID (not a Prop!)"},
+    onItemStart: { action: "item start" },
+    onItemFinish: { action: "item finish" },
   },
 };
